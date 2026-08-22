ad4214decb577b300b970359329fa0cb3051f5c5
Satish Kumar Peddi <ayno58261@gmail.com>
2026-08-22 09:51:49 +0530

    Add .gitignore and initial Main.py for data extraction setup

---
+++ Exp-1/Main.py
@@ -0,0 +1,17 @@
+import zipfile
+import os
+import pandas
+extract_path = "./sp500_data"
+
+# # Pass the full path directly into the ZipFile tool
+# with zipfile.ZipFile("c:\\Users\\devel\\OneDrive\\Documents\\DataWareHousing\\Exp-1\\archive.zip", "r") as zip_ref:
+#     zip_ref.extractall(extract_path)
+    
+# print('Extraction complete')
+
+
+# for root,dirs, files in os.walk(extract_path):
+#     for file in files:
+#         print(os.path.join(root,file))
+
+
